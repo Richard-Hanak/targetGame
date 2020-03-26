750ba3c3800e73c42438aa91919e6d67101931bc
Richard Hanak <hanak.risko@gmail.com>
2020-03-26 17:46:08 +0100

    add build version

---
+++ build/precache-manifest.b81b3586a82982787ed498fc8f8afb9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7921bdf7610e938696be37d56b904b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875313b09da6fd78c4c6",
+    "url": "/static/css/main.8996e33c.chunk.css"
+  },
+  {
+    "revision": "b54b5c4d345a25919fb4",
+    "url": "/static/js/2.fefedcfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fefedcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875313b09da6fd78c4c6",
+    "url": "/static/js/main.0d85d0d1.chunk.js"
+  },
+  {
+    "revision": "55bb46591df1bff87e04",
+    "url": "/static/js/runtime-main.c143a85f.js"
+  },
+  {
+    "revision": "450f757ba22bf3eb9f839eb26287398a",
+    "url": "/static/media/SpaceMono-Bold.450f757b.ttf"
+  },
+  {
+    "revision": "52eb73dbce8bf4083a59acc2e91699ad",
+    "url": "/static/media/SpaceMono-Regular.52eb73db.ttf"
+  }
+]);
